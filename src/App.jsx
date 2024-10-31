d51105d5f1ad960b75e62960d53c09b4f730e45a
Ulises Pandeles <pandeles.ulises@gmail.com>
2024-10-31 08:23:41 -0300

    cumple con la preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar' 
 import ItemListContainer from './components/ItemListContainer'
-import Cards from './components/Cards'
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,12 +9,7 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <ItemListContainer greeting='Holis Perris'/>
-      <div className='cardsGalery'>
-        <Cards tittle='titulo verde' src='public\images\verde.jpg' text='descripcion imagen verde'/>
-        <Cards tittle='titulo naranja' src='public\images\naranja.jpg' text='descripcion imagen naranja'/>
-        <Cards tittle='titulo azul' src='public\images\azuk.jpg' text='descripcion imagen azul'/>
-      </div>
+      <ItemListContainer greeting='Bienvenidos a Sakura'/>
     </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,27 @@
+import Cards from '../components/Cards'
 const ItemListContainer= ({greeting}) =>{
     return(
-        <div>
-            <h1>{greeting}</h1>
-        </div>
+        <>
+            <h1 className="greeting">{greeting}</h1>
+            <h3 className='subtittle'>Cafe en saquito</h3>
+            <section className='cardsGalery cafes animate__animated animate__fadeInLeft'>
+                <Cards tittle='titulo verde' src='public\images\verde.jpg' text='descripcion imagen verde'/>
+                <Cards tittle='titulo naranja' src='public\images\naranja.jpg' text='descripcion imagen naranja'/>
+                <Cards tittle='titulo azul' src='public\images\azuk.jpg' text='descripcion imagen azul'/>
+            </section>
+            <h3 className='subtittle'>Merch</h3>
+            <section className='cardsGalery merch animate__animated animate__fadeInLeft'>
+                <Cards tittle='titulo verde' src='public\images\verde.jpg' text='descripcion imagen verde'/>
+                <Cards tittle='titulo naranja' src='public\images\naranja.jpg' text='descripcion imagen naranja'/>
+                <Cards tittle='titulo azul' src='public\images\azuk.jpg' text='descripcion imagen azul'/>
+            </section>
+            <h3 className='subtittle'>Ropa</h3>
+            <section className='cardsGalery ropa animate__animated animate__fadeInLeft'>
+                <Cards tittle='titulo verde' src='public\images\verde.jpg' text='descripcion imagen verde'/>
+                <Cards tittle='titulo naranja' src='public\images\naranja.jpg' text='descripcion imagen naranja'/>
+                <Cards tittle='titulo azul' src='public\images\azuk.jpg' text='descripcion imagen azul'/>
+            </section>
+        </>
     )
 }
 
