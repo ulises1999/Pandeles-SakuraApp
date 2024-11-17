2b7e2d543fe18ff875f9ba8731c71856664fd0a3
Ulises Pandeles <pandeles.ulises@gmail.com>
2024-11-17 20:25:25 -0300

    Id no definido en categorias

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -3,13 +3,17 @@ import {getOneProducto } from "../mock/Data"
 import ItemDetail from './ItemDetail'
 import {useParams} from 'react-router-dom'
 
-const ItemDetailContainer = ()=>{
+const ItemDetailContainer = ({greeting})=>{
     const[producto,setProducto]= useState({})
-    const { id }= useParams()
-    
-    console.log (id)
+    const { id}= useParams()
+    console.log(useParams())
+
 
     useEffect(()=>{
+        if (!id) {
+            console.log("ID no definido");
+            return;
+        }
         getOneProducto(id)
         .then((res)=>setProducto(res))
         .catch((error)=>console.log(error))
@@ -17,6 +21,7 @@ const ItemDetailContainer = ()=>{
     
     return (
         <div>
+            <h1>{greeting}</h1>
             <ItemDetail producto={producto}/>
         </div>
     )
